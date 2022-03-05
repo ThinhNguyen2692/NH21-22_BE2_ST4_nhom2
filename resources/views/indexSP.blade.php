9c43c0f8ce4854ecf5e6611755fd5c42e0e5c3b8
ThinhNguyen2692 <nndthinh2802@gmail.com>
2022-03-05 22:35:05 +0700

    be2

---
+++ resources/views/indexSP.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        </style>
+    </head>
+    <body class="antialiased">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/index">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/indexGT">Gioi thieu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/indexSP">san pham</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/indexLH">lien he</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/indexBG">ban ghe</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+            <div>Đây là sản phẩm</div>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test', function () {
+    return view('test');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/indexGT', function () {
+    return view('indexGT');
+});
+Route::get('/indexSP', function () {
+    return view('indexSP');
+});
+Route::get('/indexBG', function () {
+    return view('indexBG');
+});
+Route::get('/indexLH', function () {
+    return view('indexLH');
+});
+Route::get('/shopindex', function () {
+    return view('shopindex');
+});
+Route::get('/shop-product-list', function () {
+    return view('/shop-product-list');
+});
+*/
+Route::get('/{name?}', function ($name="shop-index") {
+    return view($name);
+});
