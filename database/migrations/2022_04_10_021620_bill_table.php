503fa1d98946e696da54f860084040980be52cdc
thanhbac1123 <73510814+thanhbac1123@users.noreply.github.com>
2022-04-10 10:37:10 +0700

    tuan7

---
+++ database/migrations/2022_04_10_021620_bill_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('id_product');
+            $table->string('id_user');
+            $table->string('quantity');
+            $table->integer('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
