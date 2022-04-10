44948bca57178b80a9f73df5d4ea81a751a30de5
thanhbac1123 <73510814+thanhbac1123@users.noreply.github.com>
2022-04-10 10:39:58 +0700

    tuan7

---
+++ database/migrations/2022_04_10_021608_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('address', 150);
+            $table->string('email', 100);
+            $table->string('phone_number', 10);
+            $table->integer('role_id');
+            $table->string('pass_word', 150);
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
